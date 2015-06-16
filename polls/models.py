1a8beaca804523fca2eaf26c33a17de3d0a80339
Elijah Grubb <theophilus@outlook.com>
2015-06-15 23:12:52 -0600

    created the admin side to our polls app

--- polls/models.py
+++ polls/models.py
@@ -11,8 +11,15 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 
+	def __unicode__(self):
+		return self.question_text
+
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
@@ -20,4 +27,7 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 	
 	def __str__(self):
+		return self.choice_text
+
+	def __unicode__(self):
 		return self.choice_text
